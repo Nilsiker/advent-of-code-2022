df78253084fe5f4671ee13ee43d54d2632044fac
Nilsiker <nilsiker94@gmail.com>
2022-12-06 11:12:05 +0100

    scrapped unique() snippet found online
    instead made a more Andreas-y solution

--- 2022/src/bin/6.rs
+++ 2022/src/bin/6.rs
@@ -1,29 +1,28 @@
+use std::collections::HashSet;
+
 use advent_of_code_2022::read_input_string;
 
 fn main() {
     let data = read_input_string(6);
     let packet_marker_length = 4;
     let message_marker_length = 14;
+    
     for i in 0..data.chars().count() {
-        if let None = unique(&data[i..i + packet_marker_length]) {
-            println!("First packet marker at {}", i + packet_marker_length);
+        let candidate_marker = &data[i..i + packet_marker_length]
+            .chars()
+            .collect::<HashSet<char>>();
+        if candidate_marker.len() == packet_marker_length {
+            println!("First message marker at {}", i + packet_marker_length);
             break;
         }
     }
     for i in 0..data.chars().count() {
-        if let None = unique(&data[i..i + message_marker_length]) {
+        let candidate_marker = &data[i..i + message_marker_length]
+            .chars()
+            .collect::<HashSet<char>>();
+        if candidate_marker.len() == message_marker_length {
             println!("First message marker at {}", i + message_marker_length);
             break;
         }
     }
-}
-
-fn unique(s: &str) -> Option<(usize, usize, char)> {
-    s.chars().enumerate().find_map(|(i, c)| {
-        s.chars()
-            .enumerate()
-            .skip(i + 1)
-            .find(|(_, other)| c == *other)
-            .map(|(j, _)| (i, j, c))
-    })
 }
