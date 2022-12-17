fc82787129474bc3e37103b8a70745b4c8ca7a96
Andreas Nilsson <nilsiker94@gmail.com>
2022-12-17 19:56:24 +0100

    solved d12p2, flattened map data

--- 2022/src/bin/12.rs
+++ 2022/src/bin/12.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BinaryHeap, HashSet, VecDeque},
+    collections::{HashSet, VecDeque},
     time::SystemTime,
     usize,
 };
@@ -9,22 +9,53 @@ use advent_of_code_2022::read_input_lines;
 fn main() {
     let now = SystemTime::now();
     let data = read_input_lines(12);
-    let MapData { map, start, end } = get_map(data);
+    let MapData {
+        grid: map,
+        width,
+        start,
+        end,
+    } = get_map(data);
 
-    let steps = dijkstra(&map, start, |x, y| end.0 == x && end.1 == y);
-    println!(
-        "The highest peak {}",
-        match steps {
-            None => format!("can't be reached."),
-            Some(steps) => format!("can be reached in {steps} steps."),
-        }
+    let steps = dijkstra(
+        &map,
+        width,
+        start,
+        |x, y, nx, ny| map[ny * width + nx] <= map[y * width + x].saturating_add(1),
+        |x, y| end.0 == x && end.1 == y,
     );
+    
+    match steps {
+        None => println!("The highest peak can't be reached."),
+        Some(steps) => println!("The highest peak can be reached in {steps} steps."),
+    };
+
+    // part 2
+    let steps = dijkstra(
+        &map,
+        width,
+        end,
+        |x, y, nx, ny| map[ny * width + nx] >= map[y * width + x].saturating_sub(1),
+        |x, y| map[y * width + x] == 'a' as usize,
+    );
+    
+    match steps {
+        None => println!("The highest peak can't be reached."),
+        Some(steps) => println!("From closest 'a' elevation, the highest peak can be reached in {steps} steps."),
+    };
+
     println!("{:?}", now.elapsed().unwrap());
 }
 
-fn dijkstra<F>(map: &Vec<Vec<usize>>, start: (usize, usize), done_condition: F) -> Option<usize>
+fn dijkstra<F, S>(
+    map: &Vec<usize>,
+    width: usize,
+    start: (usize, usize),
+    step_condition: S,
+    done_condition: F,
+) -> Option<usize>
 where
     F: Fn(usize, usize) -> bool,
+    S: Fn(usize, usize, usize, usize) -> bool,
 {
     let mut queue = VecDeque::new();
     let mut visited = HashSet::new();
@@ -32,34 +63,32 @@ where
     queue.push_back((0, start));
     visited.insert(start);
 
-    let mut result = None;
-
     while let Some((dist, (x, y))) = queue.pop_front() {
-        println!("on ({x},{y})={} - current total cost {dist}", char::from_u32(map[y][x] as u32).unwrap());
         for (nx, ny) in get_cardinals(
             (x as isize, y as isize),
-            map[0].len() as isize,
-            map.len() as isize,
+            width as isize,
+            map.len() as isize / width as isize,
         ) {
-            if map[ny][nx] <= map[y][x].saturating_add(1) && visited.insert((nx, ny)) {
-                if done_condition(nx, ny) && result.unwrap_or(usize::MAX) > dist+1 {
-                    result = Some(dist + 1);
+            if step_condition(x, y, nx, ny) && visited.insert((nx, ny)) {
+                if done_condition(nx, ny) {
+                    return Some(dist + 1);
                 }
-                let d = if map[ny][nx] == 'a' as usize {0} else {dist+1};
-                queue.push_back((d, (nx, ny)))
+                queue.push_back((dist + 1, (nx, ny)))
             }
         }
     }
 
-    result
+    None
 }
 
 fn get_map(data: Vec<String>) -> MapData {
-    let mut grid = vec![];
     let mut start = (0, 0);
     let mut end = (0, 0);
+    let width = data[0].len();
+    let mut nested_grid = vec![];
+
     for (y, line) in data.iter().enumerate() {
-        grid.push(
+        nested_grid.push(
             line.chars()
                 .enumerate()
                 .map(|(x, char)| match char {
@@ -73,12 +102,14 @@ fn get_map(data: Vec<String>) -> MapData {
                     }
                     _ => char as usize,
                 })
-                .collect(),
+                .collect::<Vec<usize>>(),
         );
     }
+    let grid = nested_grid.into_iter().flatten().collect::<Vec<usize>>();
 
     MapData {
-        map: grid,
+        grid,
+        width,
         start,
         end,
     }
@@ -96,12 +127,8 @@ fn get_cardinals((x, y): (isize, isize), x_bound: isize, y_bound: isize) -> Vec<
 
 #[derive(Debug)]
 struct MapData {
-    map: Vec<Vec<usize>>,
+    grid: Vec<usize>,
+    width: usize,
     start: (usize, usize),
     end: (usize, usize),
-}
-impl MapData {
-    fn get(&self, x: usize, y: usize) -> usize {
-        self.map[y][x]
-    }
 }
