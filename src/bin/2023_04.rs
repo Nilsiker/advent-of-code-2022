8c9f9201244c0a642bebd317bb2b665477e3f775
Nilsiker <nilsiker94@gmail.com>
2023-12-04 20:22:21 +0100

    optimize day 4 solution

--- src/bin/2023_04.rs
+++ src/bin/2023_04.rs
@@ -1,5 +1,3 @@
-// NOTE Ok, this takes about 4s on my machine - that's HEAVY! Surely, there's a better way.
-
 
 use advent_of_code::read_input_lines;
 
@@ -8,55 +6,34 @@ fn main() {
 
     let cards: Vec<Card> = read_input_lines(2023, 4).iter().map(Card::from).collect();
     let score_sum: usize = cards.iter().map(|card| card.score()).sum();
-    let mut copies: Vec<Card> = vec![];
-    let mut copies_count = 0;
 
-    for card in &cards {
-        let winning_numbers = card.winning_numbers();
-        for i in 1..=winning_numbers {
-            if let Some(card) = cards.get(card.id + i - 1) {
-                copies_count += 1;
-                copies.push(card.clone())
-            }
-        }
+    let mut card_counters = vec![1usize; cards.len()];
+
+    for i in 0..card_counters.len() {
+        let this_card_count = card_counters[i];
+        let winning_numbers = cards[i].winning_numbers();
+        let last_index = (i + winning_numbers).min(cards.len());
+        card_counters[i + 1..=last_index]
+            .iter_mut()
+            .for_each(|count| *count += this_card_count);
     }
-
-    while let Some(copy) = copies.pop() {
-        let winning_numbers = copy.winning_numbers();
-
-        for i in 1..=winning_numbers {
-            if let Some(card) = cards.get(copy.id + i - 1) {
-                copies_count += 1;
-                copies.push(card.clone())
-            }
-        }
-    }
-
+    
+    let cards_sum: usize = card_counters.iter().map(|count| count).sum();
     let elapsed = start.elapsed();
 
     println!("Part A would give a score sum of {score_sum:#?}.");
-    println!(
-        "Part B would give a total of {} cards.",
-        cards.len() + copies_count
-    );
+    println!("Part B would give a total of {} cards.", cards_sum);
     println!("{elapsed:?}");
 }
 
 #[derive(Debug, Clone)]
 struct Card {
-    id: usize,
     key: Vec<usize>,
     numbers: Vec<usize>,
 }
 impl From<&String> for Card {
     fn from(value: &String) -> Self {
         let colon_index = value.find(':').expect("colon exists");
-        let id = value[0..colon_index]
-            .split(' ')
-            .next_back()
-            .expect("expect an id after Card")
-            .parse()
-            .expect("parseable to usize");
 
         let pipe_index = value.find('|').expect("pipe exists");
         let key: Vec<usize> = value[colon_index + 1..pipe_index]
@@ -71,7 +48,7 @@ impl From<&String> for Card {
             .map(|str| str.parse().expect("parseable to usize"))
             .collect();
 
-        Self { id, key, numbers }
+        Self { key, numbers }
     }
 }
 impl Card {
